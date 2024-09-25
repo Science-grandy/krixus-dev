c7d24219b9bc0dbbc87a7c96ca2b61a1a622fdce
Science-grandy <sciencegrandy@gmail.com>
2024-09-25 14:14:20 +0100

    Upgraded items

--- ogss/header.php
+++ ogss/header.php
@@ -11,7 +11,7 @@
   <title><?php echo $title ?> | <?php echo $_SESSION['system']['name'] ?></title>
   <?php ob_end_flush(); ?>
 
-  <link rel="icon" type="image/x-icon" href="assets/uploads/kr8_favicon.ico">
+  <link rel="icon" type="image/svg+xml" href="assets/uploads/logo.svg">
   <!-- Google Font: Source Sans Pro -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
